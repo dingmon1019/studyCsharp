25751e998e6b90244f8fc0481a70a85c7310b2fb
dingmon1019 <jsjsjsjsjs1019@naver.com>
2023-08-21 00:02:33 +0900

    start

---
+++ study.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Threading.Tasks;
+using System.Text;
+using System.Linq ;
+using System.Collections.Generic;
+
+namespace Csharp
+{
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hi");
+            Console.WriteLine(args.Length);
+
+            Console.ReadKey();
+        }
+    }
+
+
+
+
+}
